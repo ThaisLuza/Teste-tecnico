8149079e190c3086fe0b3873bea19d4af1e28f72
Thais Luza <thaisluza21@hotmail.com>
2024-08-27 21:43:06 -0300

    iniciando projeto

---
+++ dist/config/database.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const pool = new pg_1.Pool({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432", 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+exports.default = pool;
